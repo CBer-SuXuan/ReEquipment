f999eb3e550fbf289dfa9d06bfeb72a4d12151a2
SuXuan_Dev <1755719577@qq.com>
2026-04-21 22:30:43 +0800

    Clear config.yml

--- src/main/java/org/reuac/reequipment/ReEquipment.java
+++ src/main/java/org/reuac/reequipment/ReEquipment.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -16,172 +17,163 @@ import org.reuac.reequipment.commands.ReclCommand;
 import org.reuac.reequipment.listeners.EntityDamageListener;
 import org.reuac.reequipment.model.Level;
 import org.reuac.reequipment.utils.ConfigUtils;
-import org.reuac.reequipment.utils.ItemUtils;
 
+import java.io.File;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReEquipment extends JavaPlugin implements Listener {
 
-    public static FileConfiguration config;
-    public static String prefix;
-    public static Map<String, Sound> sounds = new HashMap<>();
-    public static Map<String, String> messages = new HashMap<>();
-    public static boolean broadcastEnabled;
-    public static List<String> broadcastMessages;
-    public static ItemStack temperingTool;
-    public static ItemStack luckyTool;
-    public static int luckValue;
-    public static Map<String, String> equipmentTypes = new HashMap<>();
-    public static Map<String, List<String>> equipmentTypeMaterials = new HashMap<>();
-    public static TreeMap<Integer, Level> levels = new TreeMap<>();
-    public static Map<String, String> itemNames = new HashMap<>();
-    public static int broadcastAllowLevel;
+	public static FileConfiguration config;
+	public static FileConfiguration prdConfig; // PRD配置对象
+	public static File prdFile;                // PRD文件对象
+	public static String prefix;
+	public static Map<String, Sound> sounds = new HashMap<>();
+	public static Map<String, String> messages = new HashMap<>();
+	public static boolean broadcastEnabled;
+	public static List<String> broadcastMessages;
+	public static ItemStack temperingTool;
+	public static Map<String, String> equipmentTypes = new HashMap<>();
+	public static Map<String, List<String>> equipmentTypeMaterials = new HashMap<>();
+	public static TreeMap<Integer, Level> levels = new TreeMap<>();
+	public static Map<String, String> itemNames = new HashMap<>();
+	public static int broadcastAllowLevel;
 
-    public static List<String> bottomLores = new ArrayList<>();
+	public static List<String> bottomLores = new ArrayList<>();
 
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
-        reloadConfiguration();
-        getCommand("recl").setExecutor(new ReclCommand(this));
-        getServer().getPluginManager().registerEvents(new EntityDamageListener(this), this);
-        getLogger().info("reEquipment 启动!");
-    }
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		saveResource("prd.yml", false); // 加载 prd.yml
+		reloadConfiguration();
+		getCommand("recl").setExecutor(new ReclCommand(this));
+		getServer().getPluginManager().registerEvents(new EntityDamageListener(this), this);
+		getLogger().info("reEquipment 启动!");
+	}
 
-    @Override
-    public void onDisable() {
-        getLogger().info("reEquipment 关闭!");
-    }
+	@Override
+	public void onDisable() {
+		getLogger().info("reEquipment 关闭!");
+	}
 
-    public void reloadConfiguration() {
-        reloadConfig();
-        config = getConfig();
+	public void reloadConfiguration() {
+		reloadConfig();
+		config = getConfig();
 
-        prefix = ChatColor.translateAlternateColorCodes('&', config.getString("Prefix", "&7[&d淬炼&7] "));
+		prdFile = new File(getDataFolder(), "prd.yml");
+		if (!prdFile.exists()) {
+			saveResource("prd.yml", false);
+		}
+		prdConfig = YamlConfiguration.loadConfiguration(prdFile);
 
-        sounds.clear();
-        sounds.put("downgrade", ConfigUtils.getSound(config.getString("Sound.downgrade")));
-        sounds.put("defeat", ConfigUtils.getSound(config.getString("Sound.defeat")));
+		prefix = ChatColor.translateAlternateColorCodes('&', config.getString("Prefix", "&7[&d淬炼&7] "));
 
-        bottomLores.clear();
-        bottomLores = config.getStringList("bottomLores");
+		sounds.clear();
+		sounds.put("success", ConfigUtils.getSound(config.getString("Sound.success")));
+		sounds.put("downgrade", ConfigUtils.getSound(config.getString("Sound.downgrade")));
 
-        messages.clear();
-        messages.put("openTempering", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.openTempering")));
-        messages.put("startTempering", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.startTempering")));
-        messages.put("cannotTempering", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.cannotTempering")));
-        messages.put("emptyItem", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.emptyItem")));
-        messages.put("noType", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.noType")));
-        messages.put("noMoney", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.noMoney")));
-        messages.put("noMaterial", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.noMaterial")));
-        messages.put("success", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.success")));
-        messages.put("drop", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.drop")));
-        messages.put("defeat", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.defeat")));
+		bottomLores.clear();
+		bottomLores = config.getStringList("bottomLores");
 
-        broadcastEnabled = config.getBoolean("BroadCast.enable", true);
-        broadcastMessages = config.getStringList("BroadCast.messages").stream()
-                .map(s -> ChatColor.translateAlternateColorCodes('&', s))
-                .collect(Collectors.toList());
+		messages.clear();
+		messages.put("openTempering", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.openTempering")));
+		messages.put("startTempering", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.startTempering")));
+		messages.put("cannotTempering", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.cannotTempering")));
+		messages.put("emptyItem", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.emptyItem")));
+		messages.put("noType", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.noType")));
+		messages.put("noMoney", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.noMoney")));
+		messages.put("noMaterial", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.noMaterial")));
+		messages.put("success", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.success")));
+		messages.put("drop", prefix + ChatColor.translateAlternateColorCodes('&', config.getString("Messages.drop")));
 
-        broadcastAllowLevel = config.getInt("BroadCast.allowLevel", 0);
+		broadcastEnabled = config.getBoolean("BroadCast.enable", true);
+		broadcastMessages = config.getStringList("BroadCast.messages").stream()
+				.map(s -> ChatColor.translateAlternateColorCodes('&', s))
+				.collect(Collectors.toList());
 
-        if (config.getBoolean("Article.temperingTools.setNormal")){
-            String materialType = config.getString("Article.temperingTools.materialType");
+		broadcastAllowLevel = config.getInt("BroadCast.allowLevel", 0);
 
-            try {
-                temperingTool = new ItemStack(Material.valueOf(materialType));
-            } catch (IllegalArgumentException e) {
-                getLogger().warning("未知的物品类型: " + materialType);
-                temperingTool = new ItemStack(Material.STONE);
-            }
-        }else {
-            temperingTool = loadItemStackFromConfig(config, "Article.temperingTools");
-        }
+		if (config.getBoolean("Article.temperingTools.setNormal")) {
+			String materialType = config.getString("Article.temperingTools.materialType");
 
-        if (config.getBoolean("Article.LuckyTools.setNormal")){
-            String materialType = config.getString("Article.LuckyTools.materialType");
+			try {
+				temperingTool = new ItemStack(Material.valueOf(materialType));
+			} catch (IllegalArgumentException e) {
+				getLogger().warning("未知的物品类型: " + materialType);
+				temperingTool = new ItemStack(Material.STONE);
+			}
+		} else {
+			temperingTool = loadItemStackFromConfig(config, "Article.temperingTools");
+		}
 
-            try {
-                luckyTool = new ItemStack(Material.valueOf(materialType));
-            } catch (IllegalArgumentException e) {
-                getLogger().warning("未知的物品类型: " + materialType);
-                luckyTool = new ItemStack(Material.STONE);
-            }
-        }else {
-            luckyTool = loadItemStackFromConfig(config, "Article.LuckyTools");
-        }
+		equipmentTypes.clear();
+		equipmentTypeMaterials.clear();
+		ConfigurationSection typeSection = config.getConfigurationSection("Type");
+		if (typeSection != null) {
+			for (String typeName : typeSection.getKeys(false)) {
+				String effectiveArea = typeSection.getString(typeName + ".effectiveArea", "MainHand");
+				List<String> materials = typeSection.getStringList(typeName + ".types");
+				equipmentTypes.put(typeName, effectiveArea);
+				equipmentTypeMaterials.put(typeName, materials);
+			}
+		}
 
-        luckValue = config.getInt("Article.LuckyTools.luckValue", 0);
+		levels.clear();
+		for (String levelKey : config.getConfigurationSection("Level").getKeys(false)) {
+			int levelNumber;
+			try {
+				levelNumber = Integer.parseInt(levelKey);
+			} catch (NumberFormatException e) {
+				getLogger().warning("无效的等级: " + levelKey);
+				continue;
+			}
+			levels.put(levelNumber, ConfigUtils.loadLevelFromConfig(config, levelNumber));
+		}
 
-        equipmentTypes.clear();
-        equipmentTypeMaterials.clear();
-        ConfigurationSection typeSection = config.getConfigurationSection("Type");
-        if (typeSection != null) {
-            for (String typeName : typeSection.getKeys(false)) {
-                String effectiveArea = typeSection.getString(typeName + ".effectiveArea", "MainHand");
-                List<String> materials = typeSection.getStringList(typeName + ".types");
-                equipmentTypes.put(typeName, effectiveArea);
-                equipmentTypeMaterials.put(typeName, materials);
-            }
-        }
+		// 加载 ItemName 配置
+		itemNames.clear();
+		ConfigurationSection itemNameSection = config.getConfigurationSection("ItemName");
+		if (itemNameSection != null) {
+			for (String materialName : itemNameSection.getKeys(false)) {
+				itemNames.put(materialName, ChatColor.translateAlternateColorCodes('&', itemNameSection.getString(materialName)));
+			}
+		}
+	}
 
-        levels.clear();
-        for (String levelKey : config.getConfigurationSection("Level").getKeys(false)) {
-            int levelNumber;
-            try {
-                levelNumber = Integer.parseInt(levelKey);
-            } catch (NumberFormatException e) {
-                getLogger().warning("无效的等级: " + levelKey);
-                continue;
-            }
-            levels.put(levelNumber, ConfigUtils.loadLevelFromConfig(config, levelNumber));
-        }
-
-        // 加载 ItemName 配置
-        itemNames.clear();
-        ConfigurationSection itemNameSection = config.getConfigurationSection("ItemName");
-        if (itemNameSection != null) {
-            for (String materialName : itemNameSection.getKeys(false)) {
-                itemNames.put(materialName, ChatColor.translateAlternateColorCodes('&', itemNameSection.getString(materialName)));
-            }
-        }
-    }
-
-    public void playSound(Player player, String soundKey) {
-        Sound sound = sounds.get(soundKey);
-        if (sound != null) {
-            player.playSound(player.getLocation(), sound, 1.0f, 1.0f);
-        }
-    }
+	public void playSound(Player player, String soundKey) {
+		Sound sound = sounds.get(soundKey);
+		if (sound != null) {
+			player.playSound(player.getLocation(), sound, 1.0f, 1.0f);
+		}
+	}
 
 
-    private ItemStack loadItemStackFromConfig(FileConfiguration config, String path) {
-        String displayName = ChatColor.translateAlternateColorCodes('&', config.getString(path + ".displayName", ""));
-        String materialType = config.getString(path + ".materialType", "STONE");
-        List<String> lores = config.getStringList(path + ".lores").stream()
-                .map(s -> ChatColor.translateAlternateColorCodes('&', s))
-                .collect(Collectors.toList());
-        boolean glow = config.getBoolean(path + ".glow", false);
+	private ItemStack loadItemStackFromConfig(FileConfiguration config, String path) {
+		String displayName = ChatColor.translateAlternateColorCodes('&', config.getString(path + ".displayName", ""));
+		String materialType = config.getString(path + ".materialType", "STONE");
+		List<String> lores = config.getStringList(path + ".lores").stream()
+				.map(s -> ChatColor.translateAlternateColorCodes('&', s))
+				.collect(Collectors.toList());
+		boolean glow = config.getBoolean(path + ".glow", false);
 
-        ItemStack itemStack;
-        try {
-            itemStack = new ItemStack(Material.valueOf(materialType));
-        } catch (IllegalArgumentException e) {
-            getLogger().warning("未知的物品类型: " + materialType);
-            itemStack = new ItemStack(Material.STONE);
-        }
+		ItemStack itemStack;
+		try {
+			itemStack = new ItemStack(Material.valueOf(materialType));
+		} catch (IllegalArgumentException e) {
+			getLogger().warning("未知的物品类型: " + materialType);
+			itemStack = new ItemStack(Material.STONE);
+		}
 
-        ItemMeta meta = itemStack.getItemMeta();
-        meta.setDisplayName(displayName);
-        meta.setLore(lores);
+		ItemMeta meta = itemStack.getItemMeta();
+		meta.setDisplayName(displayName);
+		meta.setLore(lores);
 
-        if (glow) {
-            meta.addEnchant(Enchantment.LUCK_OF_THE_SEA, 1, false);
-            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        }
+		if (glow) {
+			meta.addEnchant(Enchantment.LUCK_OF_THE_SEA, 1, false);
+			meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		}
 
-        itemStack.setItemMeta(meta);
-        return itemStack;
-    }
+		itemStack.setItemMeta(meta);
+		return itemStack;
+	}
 }
--- src/main/java/org/reuac/reequipment/utils/ConfigUtils.java
+++ src/main/java/org/reuac/reequipment/utils/ConfigUtils.java
@@ -16,50 +16,49 @@ import java.util.stream.Collectors;
 
 public class ConfigUtils {
 
-    public static Sound getSound(String soundName) {
-        if (soundName == null || soundName.equals("#")) {
-            return null;
-        }
-        try {
-            return Sound.valueOf(soundName.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid sound name: " + soundName);
-            return null;
-        }
-    }
+	public static Sound getSound(String soundName) {
+		if (soundName == null || soundName.equals("#")) {
+			return null;
+		}
+		try {
+			return Sound.valueOf(soundName.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			System.out.println("Invalid sound name: " + soundName);
+			return null;
+		}
+	}
 
-    public static Level loadLevelFromConfig(FileConfiguration config, int levelNumber) {
-        String path = "Level." + levelNumber;
-        int money = config.getInt(path + ".money", 0);
-        int materialAmount = config.getInt(path + ".materialAmount", 0);
-        int successRate = config.getInt(path + ".successRate", 0);
-        int downgradeRate = config.getInt(path + ".downgradeRate", 0);
+	public static Level loadLevelFromConfig(FileConfiguration config, int levelNumber) {
+		String path = "Level." + levelNumber;
+		int money = config.getInt(path + ".money", 0);
+		int materialAmount = config.getInt(path + ".materialAmount", 0);
+		int successRate = config.getInt(path + ".successRate", 0);
 
-        Map<String, LevelEffect> levelEffects = new HashMap<>();
-        ConfigurationSection effectSection = config.getConfigurationSection(path + ".effect");
-        if (effectSection != null) {
-            for (String type : effectSection.getKeys(false)) {
-                String effectPath = path + ".effect." + type;
-                // 重新添加 effectiveArea 的读取,从 ReEquipment.equipmentTypes 获取
-                String effectiveArea = ReEquipment.equipmentTypes.get(type);
-                double damageBonus = config.getDouble(effectPath + ".damageBonus", 0);
-                double defense = config.getDouble(effectPath + ".defense", 0);
-                // 将 effectiveArea 传递给 LevelEffect 的构造函数
-                levelEffects.put(type, new LevelEffect(damageBonus, defense, effectiveArea));
-            }
-        }
+		Map<String, LevelEffect> levelEffects = new HashMap<>();
+		ConfigurationSection effectSection = config.getConfigurationSection(path + ".effect");
+		if (effectSection != null) {
+			for (String type : effectSection.getKeys(false)) {
+				String effectPath = path + ".effect." + type;
+				// 重新添加 effectiveArea 的读取,从 ReEquipment.equipmentTypes 获取
+				String effectiveArea = ReEquipment.equipmentTypes.get(type);
+				double damageBonus = config.getDouble(effectPath + ".damageBonus", 0);
+				double defense = config.getDouble(effectPath + ".defense", 0);
+				// 将 effectiveArea 传递给 LevelEffect 的构造函数
+				levelEffects.put(type, new LevelEffect(damageBonus, defense, effectiveArea));
+			}
+		}
 
-        Map<String, LevelLores> levelLores = new HashMap<>();
-        ConfigurationSection loresSection = config.getConfigurationSection(path + ".lores");
-        if (loresSection != null) {
-            for (String type : loresSection.getKeys(false)) {
-                List<String> lores = config.getStringList(path + ".lores." + type).stream()
-                        .map(s -> ChatColor.translateAlternateColorCodes('&', s))
-                        .collect(Collectors.toList());
-                levelLores.put(type, new LevelLores(lores));
-            }
-        }
+		Map<String, LevelLores> levelLores = new HashMap<>();
+		ConfigurationSection loresSection = config.getConfigurationSection(path + ".lores");
+		if (loresSection != null) {
+			for (String type : loresSection.getKeys(false)) {
+				List<String> lores = config.getStringList(path + ".lores." + type).stream()
+						.map(s -> ChatColor.translateAlternateColorCodes('&', s))
+						.collect(Collectors.toList());
+				levelLores.put(type, new LevelLores(lores));
+			}
+		}
 
-        return new Level(money, materialAmount, successRate, downgradeRate, levelEffects, levelLores);
-    }
+		return new Level(money, materialAmount, successRate, levelEffects, levelLores);
+	}
 }
